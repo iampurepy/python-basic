e5640c0f2a2e5d25265f9a80a3fde4bde034b90b
iampurepy <56418951+iampurepy@users.noreply.github.com>
2019-10-11 11:10:04 +0700

    Create ex.1.7-string-more.py

---
+++ ex.1.7-string-more.py
@@ -0,0 +1,27 @@
+print("Xin chao")
+print("How are you, {}".format('Thuan?'))
+#Note that, luôn luôn có {} khi sử dụng f/format
+
+print("#"*30)
+#In ra mot dong gom 30# ký tự. 
+
+"""
+From now on you’re going to break all the code you
+write or a friend’s code. I won’t have a Break It section explicitly in every exercise, but I will do this in
+almost every video. Your goal is to ﬁnd as many different ways to break your code until you get tired or
+exhaust all possibilities. In some exercises I might point out a speciﬁc common way people break that
+exercise’s code, but otherwise consider this a standing order to always break it.
+
+Common Student Questions
+Why are you using the variable named 'snow'? That’s actually not a variable: it is just a string with
+the word snow in it. A variable wouldn’t have the single-quotes around it.
+Is it normal to write an English comment for every line of code like you say to do in Study Drill 1? No,
+you write comments only to explain difficult-to-understand code or why you did something.
+Why is usually much more important, and then you try to write the code so that it explains how
+something is being done on its own. However, sometimes you have to write such nasty code
+to solve a problem that it does need a comment on every line. In this case it’s strictly for you
+to practice translating code to English.
+Can I use single-quotes or double-quotes to make a string or do they do different things? In Python
+either way to make a string is acceptable, although typically you’ll use single-quotes for any
+short strings like 'a' or 'snow'.
+"""
