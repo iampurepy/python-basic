394e5d49fe81637d07cf4bd2b6874d5bd6baf5a0
iampurepy <56418951+iampurepy@users.noreply.github.com>
2019-10-11 17:33:32 +0700

    Create ex3.2-write-content.py

---
+++ ex3.2-write-content.py
@@ -0,0 +1,27 @@
+############ WRITE CONTENT TO A FILE AFTER DELETE PREVIOUS CONTENT #####################
+from sys import argv
+pythonfile, tenfile = argv
+#OPEN FILE. COMMAND LINE MUST have two.
+target = open(tenfile, 'w')
+#DELETE ALL CONTENT PREVIOUS
+target.truncate()
+#WRITE NEW CONTENT
+content = input("Nhap noi dung muon luu vao file: ")
+target.write(content)
+target.close()
+
+"""
+1. What does 'w' mean? If you use 'w' then you’re saying “open this ﬁle in ‘write’ mode,” thus the 'w' character. There’s
+also 'r' for “read,” 'a' for “append,” and modiﬁers on these.
+
+2. What modiﬁers to the ﬁle modes can I use? The most important one to know for now is the + modi-
+ﬁer, so you can do 'w+', 'r+', and 'a+'. This will open the ﬁle in both read and write mode
+and, depending on the character use, position the ﬁle in different ways.
+
+3. Does just doing open(filename) open it in 'r' (read) mode? Yes, that’s the default for the
+open() function.
+
+4. Mở rộng code.
+target.write("\n") xuống dòng mới trước khi ghi thêm nội dung.
+target.write(content)
+"""
