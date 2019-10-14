a21cc743f0975e2abe69ac0f5d2a4039b13aba00
iampurepy <56418951+iampurepy@users.noreply.github.com>
2019-10-14 17:41:25 +0700

    Update ex6.1-more-lists-do.py

--- ex6.1-more-lists-do.py
+++ ex6.1-more-lists-do.py
@@ -1,4 +1,4 @@
-#############LUYỆN TẬP VỚI LIST -append(), join(), pop(),pop(i)#####################
+#############LUYỆN TẬP VỚI LIST -append(), join(), pop(),pop(i) ex38.py#####################
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 print("Wait there are not 10 things in that list. Let's fix that.")
 stuff = ten_things.split(' ')
